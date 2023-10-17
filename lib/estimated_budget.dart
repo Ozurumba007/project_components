f14cb962bfc9dcf8c566479c895c6615704b884a
Ozurumba Onwuasoanya <onwuasoanyaozurumba007@gmail.com>
2023-10-17 18:47:29 +0100

    removed the center widget in the code

--- lib/estimated_budget.dart
+++ lib/estimated_budget.dart
@@ -33,172 +33,170 @@ class EstimatedBudget extends StatelessWidget {
         ],
       ),
     );
-    return Center(
-      child: Container(
-        color: Colors.white,
-        height: fem * 350,
-        width: fem * 1360,
-        child: Row(
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  children: [
-                    Text(
-                      'Estimated Budget',
-                      style: TextStyle(
-                        fontSize: ffem * 42,
-                        fontWeight: FontWeight.w600,
-                      ),
+    return Container(
+      color: Colors.white,
+      height: fem * 350,
+      width: fem * 1360,
+      child: Row(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Text(
+                    'Estimated Budget',
+                    style: TextStyle(
+                      fontSize: ffem * 42,
+                      fontWeight: FontWeight.w600,
                     ),
-                  ],
-                ),
-                SizedBox(height: fem * 30),
-                Row(
-                  children: [
-                    textfieldContainer(
-                      text: 'Selling Price / Your Offer',
-                      hintText: '\$925,000',
-                      height: fem * 46,
-                      width: fem * 290,
-                    ),
-                    SizedBox(width: fem * 24),
-                    textfieldContainer(
-                      text: 'Down Payment',
-                      hintText: '20%',
-                      height: fem * 46,
-                      width: fem * 160,
-                      suffixIcon: Icon(
-                        Icons.keyboard_arrow_down,
-                        size: ffem * 30,
-                      ),
-                    ),
-                    SizedBox(width: fem * 16),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 15.0),
-                      child: Text(
-                        '=',
-                        style: TextStyle(fontSize: ffem * 45),
-                      ),
-                    ),
-                    SizedBox(width: fem * 16),
-                    textfieldContainer(
-                      text: '',
-                      height: fem * 46,
-                      width: fem * 209,
-                      hintText: '185,000.00',
-                    ),
-                  ],
-                ),
-                SizedBox(height: fem * 30),
-                Row(
-                  children: [
-                    textfieldContainer(
-                      text: 'Rate (%)',
-                      hintText: '5',
-                      height: fem * 46,
-                      width: fem * 290,
-                    ),
-                    SizedBox(width: fem * 24),
-                    textfieldContainer(
-                      text: 'Amortization Period',
-                      hintText: '30 years',
-                      height: fem * 46,
-                      width: fem * 160,
-                    ),
-                    SizedBox(width: fem * 24),
-                    textfieldContainer(
-                      text: 'Payment Frequency',
-                      hintText: 'Monthly',
-                      height: fem * 46,
-                      width: fem * 209,
-                      suffixIcon: Icon(
-                        Icons.keyboard_arrow_down,
-                        size: ffem * 30,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            SizedBox(width: fem * 40),
-            Container(
-              width: fem * 521,
-              height: fem * 172,
-              decoration: ShapeDecoration(
-                color: Color(0xFFF7FAFF),
-                shape: RoundedRectangleBorder(
-                  side: BorderSide(width: 1, color: Color(0xFFE1F2FF)),
-                  borderRadius: BorderRadius.circular(8),
-                ),
+                  ),
+                ],
               ),
-              child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          'Principal and Interest',
-                          style: TextStyle(
-                            fontSize: ffem * 16,
-                            color: textColor,
-                          ),
-                        ),
-                        Text(
-                          '\$ 3,972.48',
-                          style: TextStyle(
-                            fontSize: ffem * 16,
-                            color: textColor,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                      ],
+              SizedBox(height: fem * 30),
+              Row(
+                children: [
+                  textfieldContainer(
+                    text: 'Selling Price / Your Offer',
+                    hintText: '\$925,000',
+                    height: fem * 46,
+                    width: fem * 290,
+                  ),
+                  SizedBox(width: fem * 24),
+                  textfieldContainer(
+                    text: 'Down Payment',
+                    hintText: '20%',
+                    height: fem * 46,
+                    width: fem * 160,
+                    suffixIcon: Icon(
+                      Icons.keyboard_arrow_down,
+                      size: ffem * 30,
                     ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        propertyTaxes,
-                        Text(
-                          '\$217',
-                          style: TextStyle(
-                            fontSize: ffem * 16,
-                            color: textColor,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                      ],
+                  ),
+                  SizedBox(width: fem * 16),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 15.0),
+                    child: Text(
+                      '=',
+                      style: TextStyle(fontSize: ffem * 45),
                     ),
-                    Divider(),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text('Period Cost',
-                            style: TextStyle(
-                              fontSize: ffem * 24,
-                              fontWeight: FontWeight.w600,
-                              color: textColor,
-                            )),
-                        Text(
-                          '\$4,189.48',
+                  ),
+                  SizedBox(width: fem * 16),
+                  textfieldContainer(
+                    text: '',
+                    height: fem * 46,
+                    width: fem * 209,
+                    hintText: '185,000.00',
+                  ),
+                ],
+              ),
+              SizedBox(height: fem * 30),
+              Row(
+                children: [
+                  textfieldContainer(
+                    text: 'Rate (%)',
+                    hintText: '5',
+                    height: fem * 46,
+                    width: fem * 290,
+                  ),
+                  SizedBox(width: fem * 24),
+                  textfieldContainer(
+                    text: 'Amortization Period',
+                    hintText: '30 years',
+                    height: fem * 46,
+                    width: fem * 160,
+                  ),
+                  SizedBox(width: fem * 24),
+                  textfieldContainer(
+                    text: 'Payment Frequency',
+                    hintText: 'Monthly',
+                    height: fem * 46,
+                    width: fem * 209,
+                    suffixIcon: Icon(
+                      Icons.keyboard_arrow_down,
+                      size: ffem * 30,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          SizedBox(width: fem * 40),
+          Container(
+            width: fem * 521,
+            height: fem * 172,
+            decoration: ShapeDecoration(
+              color: Color(0xFFF7FAFF),
+              shape: RoundedRectangleBorder(
+                side: BorderSide(width: 1, color: Color(0xFFE1F2FF)),
+                borderRadius: BorderRadius.circular(8),
+              ),
+            ),
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Principal and Interest',
+                        style: TextStyle(
+                          fontSize: ffem * 16,
+                          color: textColor,
+                        ),
+                      ),
+                      Text(
+                        '\$ 3,972.48',
+                        style: TextStyle(
+                          fontSize: ffem * 16,
+                          color: textColor,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      propertyTaxes,
+                      Text(
+                        '\$217',
+                        style: TextStyle(
+                          fontSize: ffem * 16,
+                          color: textColor,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Divider(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('Period Cost',
                           style: TextStyle(
-                            color: textColor,
                             fontSize: ffem * 24,
                             fontWeight: FontWeight.w600,
-                          ),
+                            color: textColor,
+                          )),
+                      Text(
+                        '\$4,189.48',
+                        style: TextStyle(
+                          color: textColor,
+                          fontSize: ffem * 24,
+                          fontWeight: FontWeight.w600,
                         ),
-                      ],
-                    )
-                  ],
-                ),
+                      ),
+                    ],
+                  )
+                ],
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
